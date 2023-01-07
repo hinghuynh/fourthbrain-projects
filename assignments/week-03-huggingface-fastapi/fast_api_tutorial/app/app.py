285b961ef38623f85850277f47d7851156338060
hinghuynh2 <60897353+hinghuynh2@users.noreply.github.com>
2023-01-06 18:33:04 -0600

    Add week 3 homework submission.

--- assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
+++ assignments/week-03-huggingface-fastapi/fast_api_tutorial/app/app.py
@@ -1,10 +1,33 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from pyparsing import List
+from transformers import pipeline
 
 app = FastAPI()
+pipeline = pipeline(model="t5-small")
+
+
+class TextToTranslate(BaseModel):
+    input_text: str
+
+class TextsToTranslate(BaseModel):
+    input_texts: List[str]
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+
+@app.post("/echo")
+def echo(text_to_translate: TextToTranslate):
+    return {"message": text_to_translate.input_text}
+
+@app.post("/translate")
+def translate(texts_to_translate: TextsToTranslate):
+    return {"message": pipeline(texts_to_translate.input_texts)} 
     
 if __name__ == "__main__":
     import uvicorn
